158de7d2d852968d54aad6509a97f7eec1ee06e5
Vinisha Venugopal <46516416+vinishavenugopal@users.noreply.github.com>
2021-09-01 09:26:29 -0700

    Update web_app.R

--- web_app.R
+++ web_app.R
@@ -4,20 +4,20 @@ library(dplyr)
 library(tidyverse)
 
 regions=c("med","lat")
-metadata<-as.data.frame(read_csv("~/metadata_labdefined_net.csv"))
-met<-metadata[c("Sex","Site of Motor Onset","Age at Death","Sample Source","Final Subject Group - Bowser lab defined")]
+metadata<-as.data.frame(read_csv("C:/Users/vinisha/Documents/metadata_labdefined_net.csv"))
+met<-metadata[c("ExternalSampleId","Sex","Site of Motor Onset","Age at Death","Sample Source","Final Subject Group - Bowser lab defined")]
 
 server <- function(input,output,session)
-  {
+{
   
   #Selecting datasets (region of interest)
   data <- reactive({
     db = data.frame()#declaring a dataframe
     dataset <- input$selected_dataset #assigning the user input to DATASET
     if (dataset == "med")
-      db = as.data.frame(read_csv("~/txi_medial_result_net.csv"))
+      db = as.data.frame(read_csv("C:/Users/vinisha/Documents/txi_medial_result_net.csv"))
     else
-      db = as.data.frame(read_csv("~/txi_lateral_result_net.csv"))
+      db = as.data.frame(read_csv("C:/Users/vinisha/Documents/txi_lateral_result_net.csv"))
   })
   
   output$choose_gene = renderUI({
@@ -50,21 +50,21 @@ server <- function(input,output,session)
   
   output$bar_plot=renderPlot(
     {
-    db<- data()
-    var1= input$selected_gene
-    var2= input$selected_group
-    #grp='Final Subject Group - Bowser lab defined'
-    data_gene <- as.data.frame(db[db$gene == var1,-1])
-    t_data_gene<-as.data.frame(t(data_gene))
-    t_data_gene$ExternalSampleId <- rownames(t_data_gene)
-    colnames(t_data_gene)<-c(var1,'ExternalSampleId')
-    dat<-merge(t_data_gene,met,by='ExternalSampleId')
-    dat_ALS_HC<-dat[(dat$`Final Subject Group - Bowser lab defined`) %in% var2,]
-    ggplot(dat_ALS_HC, aes(x=`Final Subject Group - Bowser lab defined`), aes_string(y=var1)) +
-    geom_bar(stat="identity", aes_string(y=var1))+
-    geom_errorbar(aes(ymin=var1-sd, ymax=var1+sd), width=.2,
-                    position=position_dodge(.9))
-   })
+      db<- data()
+      var1= input$selected_gene
+      var2= input$selected_group
+      #grp='Final Subject Group - Bowser lab defined'
+      data_gene <- as.data.frame(db[db$gene == var1,-1])
+      t_data_gene<-as.data.frame(t(data_gene))
+      t_data_gene$ExternalSampleId <- rownames(t_data_gene)
+      colnames(t_data_gene)<-c(var1,'ExternalSampleId')
+      dat<-merge(t_data_gene,met,by='ExternalSampleId')
+      dat_ALS_HC<-dat[(dat$`Final Subject Group - Bowser lab defined`) %in% var2,]
+      ggplot(dat_ALS_HC, aes(x=`Final Subject Group - Bowser lab defined`), aes_string(y=var1)) +
+        geom_bar(stat="identity", aes_string(y=var1))+
+        geom_errorbar(aes(ymin=var1-sd, ymax=var1+sd), width=.2,
+                      position=position_dodge(.9))
+    })
 }
 
 ui <- fluidPage(
